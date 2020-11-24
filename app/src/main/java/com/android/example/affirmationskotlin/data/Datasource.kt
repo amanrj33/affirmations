057e356b368ba402f17c752cd25277cfe5a16422
Aman Raj <amanrj2914@gmail.com>
2020-11-24 18:04:16 +0530

    Initial commit

---
+++ app/src/main/java/com/android/example/affirmationskotlin/data/Datasource.kt
@@ -0,0 +1,21 @@
+package com.android.example.affirmationskotlin.data
+
+import com.android.example.affirmationskotlin.R
+import com.android.example.affirmationskotlin.model.Affirmation
+
+class Datasource {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
---
+++ app/src/main/java/com/android/example/affirmationskotlin/model/Affirmation.kt
@@ -0,0 +1,5 @@
+package com.android.example.affirmationskotlin.model
+
+data class Affirmation(val stringResourceId: Int) {
+
+}
